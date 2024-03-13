49c242f39397888b0d2a9af6cfabbaab36ee6639
seoin <seoin2389@icloud.com>
2024-03-13 23:58:46 +0900

    Add Autowire - config에서 추가한 BCryPasswordEncoder - 회원가입 페이지에서 입력한 password를 BCryPasswordEncoder를 이용해 암호화 후 DB 저장

--- security1/src/main/java/com/cos/security1/controller/IndexController.java
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,11 +1,23 @@
 package com.cos.security1.controller;
 
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // View return
 public class IndexController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
 
     @GetMapping({"","/"})
     public String index(){
@@ -31,21 +43,31 @@ public class IndexController {
 
     // 스프링 시큐리티가 해당 주소를 가져감 - SecurityConfig 파일 생성 후 작동 안 뺏어감
 
-    @GetMapping("/login")
-    public @ResponseBody String login(){
-        return "login";
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        return "loginForm";
     }
 
-    @GetMapping("/join")
-    public @ResponseBody String join(){
-        return "join";
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "joinForm";
+    }
+
+    @PostMapping("/join")
+    public String join(User user){
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+
+        userRepository.save(user);
+        // 회원가입 -> 비밀번호 암호화가 되지 않음 => 시큐리티로 로그인을 할 수 없음
+        // 이유는 패스워드가 암호화 안되었기 때문에 따라서 BcryptPasswordEncoder 사용
+        return "redirect:/loginForm";
     }
 
 
-    @GetMapping("/joinProc")
-    public @ResponseBody String joinProc(){
-        return "회원가입 완료됨!";
-    }
 
 
 }
