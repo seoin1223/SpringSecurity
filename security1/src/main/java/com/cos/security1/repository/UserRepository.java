69fa532bce0101a54d0648b1ec4cd16df40bf9d9
seoin <seoin2389@icloud.com>
2024-03-14 22:55:37 +0900

    Add  findByUsername(String username); - Jpa Query Method를 이용하여 유저객체를 반환하는 함수 추가

--- security1/src/main/java/com/cos/security1/repository/UserRepository.java
+++ security1/src/main/java/com/cos/security1/repository/UserRepository.java
@@ -8,4 +8,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 // @Repository라는 어노테이션이 없어도 IoC 됨, 이유는 JpaRepository를 상속했기 때문에
 public interface UserRepository extends JpaRepository<User,Integer> {
+
+    // findBy(규칙) -> Username (문법)
+    // select * from user where username = username
+    User findByUsername(String username);
+
+    // select * from user where email = email
+//    User findByEmail(); // Jpa query method
 }
