9a8b6e4e8f8d375aec0abfe9d80e49e34fa84f6a
seoin <seoin2389@icloud.com>
2024-03-20 23:52:46 +0900

    Debug BCrypyPasswordEncoder - 순환문제로 인해 컨포넌트 추가 생성

--- security1/src/main/java/com/cos/security1/config/oauth/CustomBCryptPasswordEncoder.java
+++ security1/src/main/java/com/cos/security1/config/oauth/CustomBCryptPasswordEncoder.java
@@ -1,2 +1,8 @@
-package com.cos.security1.config.oauth;public class CustomBCryptPasswordEncoder {
+package com.cos.security1.config.oauth;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBCryptPasswordEncoder extends BCryptPasswordEncoder {
 }
