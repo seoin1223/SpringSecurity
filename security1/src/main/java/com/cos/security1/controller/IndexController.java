8cf38ed0b69e780534902d9343db664fdda13f62
seoin <seoin2389@icloud.com>
2024-03-20 21:32:50 +0900

    add Controller method(일반, OAuth) - 일반 로그인일 경우 UserDetails 클래스에 담기는 것을 확인 - OAuth2 로그인일 경우 OAuth2USer 클래스에 담기는 것을 확인 => 개별적으로 구현하기 힘드니 이전에 생성한 PrincipalDetails가 OAuth2User 도 같이 상속받도록 할 예정

--- security1/src/main/java/com/cos/security1/controller/IndexController.java
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,12 +1,17 @@
 package com.cos.security1.controller;
 
+import com.cos.security1.config.auth.PrincipalDetails;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +25,26 @@ public class IndexController {
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping("/test/login")
+    public @ResponseBody String loginTest(Authentication authentication, @AuthenticationPrincipal PrincipalDetails userDetails){ // DI(의존성 주입)
+        System.out.println("/test/login =================");
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+        System.out.println("authentication : "+ principalDetails.getUser());
+
+        System.out.println("authentication : "+ userDetails.getUser());
+        return "세션정보 확인";
+    }
+
+    @GetMapping("/test/oauth/login")
+    public @ResponseBody String TestOAuthLogin(Authentication authentication, @AuthenticationPrincipal OAuth2User oAuth){ // DI(의존성 주입)
+        System.out.println("/test/login =================");
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        System.out.println("authentication : "+ oAuth2User.getAttributes());
+        System.out.println("oauth2User : "+ oAuth.getAttributes());
+
+        return "OAuth 세션정보 확인";
+    }
 
 
     @GetMapping({"","/"})
