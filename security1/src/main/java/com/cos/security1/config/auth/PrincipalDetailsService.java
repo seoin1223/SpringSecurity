9726076713bf9362c172caa9a395160c58d0ae35
seoin <seoin2389@icloud.com>
2024-03-20 21:30:07 +0900

    no message

--- security1/src/main/java/com/cos/security1/config/auth/PrincipalDetailsService.java
+++ security1/src/main/java/com/cos/security1/config/auth/PrincipalDetailsService.java
@@ -6,12 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 // 시큐리티 설정에서 .defaultSuccessUrl("/", true)
 // /login 요청이 오면 자동으로 UserDetailsService 타입으로 IoC 되어 있는 loadUserByUsername 함수가 실행된다
 @Service
-public class PrincipalDetailsService implements UserDetailsService {
+public class PrincipalDetailsService implements UserDetailsService{
 
     @Autowired
     private UserRepository userRepository;
