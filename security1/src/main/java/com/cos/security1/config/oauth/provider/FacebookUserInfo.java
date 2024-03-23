ec6096995042da3d7061c3d23665b5874d3ea6d5
seoin <seoin2389@icloud.com>
2024-03-23 23:52:48 +0900

    add FacebookUserInfo - 페이스북에서 받은 UserRequest를 담는 클래스 생성 - Oauth2UserInfo 를 상속받음

--- security1/src/main/java/com/cos/security1/config/oauth/provider/FacebookUserInfo.java
+++ security1/src/main/java/com/cos/security1/config/oauth/provider/FacebookUserInfo.java
@@ -2,22 +2,22 @@ package com.cos.security1.config.oauth.provider;
 
 import java.util.Map;
 
-public class GoogleUserInfo implements OAuth2UserInfo{
+public class FacebookUserInfo implements OAuth2UserInfo{
 
     private Map<String,Object> attributes; // getAttributes()
 
-    public GoogleUserInfo(Map<String,Object> attributes) {
+    public FacebookUserInfo(Map<String,Object> attributes) {
         this.attributes = attributes;
     }
 
     @Override
     public String getProviderId() {
-        return (String)attributes.get("sub");
+        return (String)attributes.get("id");
     }
 
     @Override
     public String getProvider() {
-        return "google";
+        return "facebook";
     }
 
     @Override
