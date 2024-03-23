2010c935b144b97ce03fd34e3e5a735c7e5a8180
seoin <seoin2389@icloud.com>
2024-03-23 23:54:11 +0900

    add Oauth2UserInfo - 네이버 구글, 카카오톡 등에서 response이름이 다름 - 따라서 인터페이스를 생성해서 각각의 상속받도록 함

--- security1/src/main/java/com/cos/security1/config/oauth/provider/OAuth2UserInfo.java
+++ security1/src/main/java/com/cos/security1/config/oauth/provider/OAuth2UserInfo.java
@@ -1,2 +1,8 @@
-package com.cos.security1.config.oauth.provider;public interface OAuth2UserInfo {
+package com.cos.security1.config.oauth.provider;
+
+public interface OAuth2UserInfo {
+    String getProviderId();
+    String getProvider();
+    String getEmail();
+    String getName();
 }
