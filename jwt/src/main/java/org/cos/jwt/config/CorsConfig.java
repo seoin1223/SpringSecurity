98c088b5ead2691f87d66cdb2ae90cda627217ea
seoin <seoin2389@icloud.com>
2024-03-27 22:23:34 +0900

    add CorsConfig - json를 자바스크립트에서 처리할 수 있게 설정 - 모든 ip에 대해서 허용 - 모든 header에 대해서 허용 - 모든 요청에 대해서 허용을 함 (post, put, get, delete, patch

--- jwt/src/main/java/org/cos/jwt/config/CorsConfig.java
+++ jwt/src/main/java/org/cos/jwt/config/CorsConfig.java
@@ -16,10 +16,10 @@ public class CorsConfig {
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+        config.setAllowCredentials(true); // 내 서바가 응답을 할 때 json을 자바스크립트에서 처리할 수 있게 할지를 설정한다.
+        config.addAllowedOrigin("*"); // 모든 ip에 대해서 응답을 허용
+        config.addAllowedHeader("*"); // 모든 header에 대해서 응답을 허용
+        config.addAllowedMethod("*"); // 모든 post, get, put, delete, patch 요청을 허용
         source.registerCorsConfiguration("/api/**",config);
 
         return new CorsFilter(source); // CorsFilter에 CorsConfigurationSource를 전달하여 반환
